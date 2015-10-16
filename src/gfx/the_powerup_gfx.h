ec63c3cfa0187012e87c4c29181f3dd886c583a2
fl4shk <fl4shk@users.noreply.github.com>
2015-10-16 09:22:59 -0500

    Changed graphics and added .gif file

--- src/gfx/the_powerup_gfx.h
+++ src/gfx/the_powerup_gfx.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	the_powerup_gfx, 64x32@4, 
+//	the_powerup_gfx, 80x16@4, 
 //	+ palette 16 entries, not compressed
-//	+ 32 tiles Metatiled by 2x2 not compressed
-//	Total size: 32 + 1024 = 1056
+//	+ 20 tiles Metatiled by 2x2 not compressed
+//	Total size: 32 + 640 = 672
 //
-//	Time-stamp: 2015-09-05, 09:51:07
+//	Time-stamp: 2015-10-16, 09:17:35
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_THE_POWERUP_GFX_H
 #define GRIT_THE_POWERUP_GFX_H
 
-#define the_powerup_gfxTilesLen 1024
-extern const unsigned short the_powerup_gfxTiles[512];
+#define the_powerup_gfxTilesLen 640
+extern const unsigned short the_powerup_gfxTiles[320];
 
 #define the_powerup_gfxPalLen 32
 extern const unsigned short the_powerup_gfxPal[16];
