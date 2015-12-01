463aca1308c1fe1d758f95c3efa118c8c9c3910c
fl4shk <fl4shk@users.noreply.github.com>
2015-11-30 19:39:09 -0600

    Updated the 8x16 font.

---
+++ src/gfx/text_8x16_gfx.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(text_8x16_gfx)
+
+//======================================================================
+//
+//	text_8x16_gfx, 128x96@4, 
+//	+ palette 16 entries, not compressed
+//	+ 192 tiles Metatiled by 1x2 not compressed
+//	Total size: 32 + 6144 = 6176
+//
+//	Time-stamp: 2015-11-30, 19:38:53
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TEXT_8X16_GFX_H
+#define GRIT_TEXT_8X16_GFX_H
+
+#define text_8x16_gfxTilesLen 6144
+extern const unsigned short text_8x16_gfxTiles[3072];
+
+#define text_8x16_gfxPalLen 32
+extern const unsigned short text_8x16_gfxPal[16];
+
+#endif // GRIT_TEXT_8X16_GFX_H
+
+//}}BLOCK(text_8x16_gfx)
