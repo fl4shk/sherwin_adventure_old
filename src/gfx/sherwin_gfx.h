a5b9033918d5225921463d0607542ec785ca39e9
fl4shk <fl4shk@users.noreply.github.com>
2016-01-12 12:41:09 -0600

    Better colors for Sherwin's hammer

--- src/gfx/sherwin_gfx.h
+++ src/gfx/sherwin_gfx.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	sherwin_gfx, 128x128@4, 
+//	sherwin_gfx, 128x160@4, 
 //	+ palette 16 entries, not compressed
-//	+ 256 tiles Metatiled by 4x4 not compressed
-//	Total size: 32 + 8192 = 8224
+//	+ 320 tiles Metatiled by 4x4 not compressed
+//	Total size: 32 + 10240 = 10272
 //
-//	Time-stamp: 2016-01-12, 08:32:17
+//	Time-stamp: 2016-01-12, 12:37:10
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_SHERWIN_GFX_H
 #define GRIT_SHERWIN_GFX_H
 
-#define sherwin_gfxTilesLen 8192
-extern const unsigned short sherwin_gfxTiles[4096];
+#define sherwin_gfxTilesLen 10240
+extern const unsigned short sherwin_gfxTiles[5120];
 
 #define sherwin_gfxPalLen 32
 extern const unsigned short sherwin_gfxPal[16];
