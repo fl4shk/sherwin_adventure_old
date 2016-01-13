0b2c22865c1b181f6b0822d2c633a6c9b2d64fc6
fl4shk <fl4shk@users.noreply.github.com>
2016-01-13 17:15:43 -0600

    Bigger hammer swing graphics

--- src/gfx/sherwin_gfx.h
+++ src/gfx/sherwin_gfx.h
@@ -8,7 +8,7 @@
 //	+ 320 tiles Metatiled by 4x4 not compressed
 //	Total size: 32 + 10240 = 10272
 //
-//	Time-stamp: 2016-01-12, 17:19:19
+//	Time-stamp: 2016-01-13, 17:15:19
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
