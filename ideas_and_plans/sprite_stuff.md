94279532ba66d9f2cab80680cfb5f07227e178a1
fl4shk <fl4shk@users.noreply.github.com>
2016-06-19 19:12:52 -0500

    updated makefiles

---
+++ ideas_and_plans/sprite_stuff.md
@@ -0,0 +1,7 @@
+Sprite Stuff
+============
+
+Instead of building an array of active sprites every frame, during the
+sprite updater functions, add a pointer to the currently processed sprite
+to an array.
+
--- makefile_devkitARM.mk
+++ makefile_devkitARM.mk
@@ -45,11 +45,11 @@ ifdef DEBUG
 	##DEBUG_FLAGS=-gdwarf-2 -ggdb -gstrict-dwarf -g
 	DEBUG_FLAGS=-gdwarf-3 -g
 	
-	#GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -O3 -g
+	#GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -O2 -g
 	#GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -Og -g
 	GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -O1 -g
 else
-	GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -O3
+	GLOBAL_BASE_FLAGS=-mcpu=arm7tdmi -mtune=arm7tdmi -I$(DEVKITPRO)/libgba/include -nostartfiles -O2
 endif
 
 
