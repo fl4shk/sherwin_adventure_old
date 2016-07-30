e0384f961e257abc115e48a02130954603d5ad1d
fl4shk <fl4shk@users.noreply.github.com>
2016-07-30 17:48:14 -0500

    updated updater script

--- util/update_non_main_source_files.sh
+++ util/update_non_main_source_files.sh
@@ -3,9 +3,17 @@
 
 #rsync -avh --progress --dry-run --existing "$1"/src .
 #rsync -avh --progress --dry-run --existing "$1"/src .
-rsync -avuh --progress --exclude=".git" --exclude="src/main.thumb.cpp" \
-	--exclude=".*.swp" --exclude=".*.swo" --existing "$1"/ .
+#rsync -avuh --progress --exclude=".git" --exclude="src/main.thumb.cpp" \
+#	--exclude=".*.swp" --exclude=".*.swo" --existing "$1"/ .
 
+rsync -avuh --progress \
+	--exclude="README.md" \
+	--exclude=".git" \
+	--exclude="src/main.thumb.cpp" \
+	--exclude="src/tags" --exclude="*types_*.taghl" \
+	--exclude="tags" --exclude="*types_*.taghl" \
+	--exclude=".*.swp" --exclude=".*.swo" \
+	--existing "$1"/ .
 
 find src -type f -exec sed -i 's/GBA Project Template/Sherwin'\''s Adventure/g' {} +
 
