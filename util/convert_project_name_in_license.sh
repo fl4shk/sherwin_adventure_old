feaaeb307c68a34e43933e727604421dda912c41
fl4shk <fl4shk@users.noreply.github.com>
2016-07-31 12:16:19 -0500

    util/convert_project_name_in_license.sh

--- util/convert_project_name_in_license.sh
+++ util/convert_project_name_in_license.sh
@@ -2,10 +2,20 @@
 
 # Man I love the find program.
 
-find . -type d \( -path ./.git -o -path ./small_code_testing_things \
-	-o -path ./deps -o -path ./objs -o -path ./asmouts \) -prune \
+find . \( -path ./.git -o -path ./small_code_testing_things \
+	-o -path ./deps -o -path ./objs -o -path ./asmouts \
+	-o -path ./lookup_tables \
 	\
-	-type f -path ./util/convert_project_name_in_license.sh -prune \
+	-o -iname "*.swp" -o -iname "*.swo" -o -iname "*.gdb_history*" \
+	-o -path ./util/convert_project_name_in_license.sh \
+	-o -iname "*tags" -o -iname "*types*.taghl" \
+	-o -iname "*.gba" -o -iname "*.elf" \
+	-o -iname "*README*" -o -iname "*COPYING*" \
+	-o -iname "*linker_map.txt*" \
+	\) -prune \
 	\
-	-type f \
+	-o -type f \
 	-exec sed -i 's/GBA Project Template/Sherwin'\''s Adventure/g' {} +
+	#-print
+
+
